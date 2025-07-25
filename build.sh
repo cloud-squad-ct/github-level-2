badbcf600979ca1d510eee8174652f26110891c3
dr0w <dr0wsec@proton.me>
2025-07-25 13:50:03 +0200

    change sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-gcc -o prog main.c
+gcc -shared -fPIC -o ./outputs/test.so test.c -ldl
+echo -e "1\nLD_PRELOAD=./test.so" > ./outputs/pr.txt
--- format.sh
+++ format.sh
@@ -1,2 +1,30 @@
-gcc -shared -fPIC -o ./outputs/test.so test.c -ldl
-echo -e "1\nLD_PRELOAD=./test.so" > ./outputs/pr.txt
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+then
+    echo "Requires file to format!"
+	exit 1
+fi
+
+if [ -z "$1" ]
+then
+    echo "Requires non empty file name!"
+	exit 1
+fi
+
+tool_name=$(basename ${1%.*})
+program_name=$(basename $(grep "Command:" $1 | cut -d" " -f3))
+valgrind_body=$(tail -n +5 $1)
+
+
+cat << EOF > output.md
+# Program Analysis
+
+Analyzed program: $program_name
+
+---
+
+## $tool_name
+
+$valgrind_body
+EOF
